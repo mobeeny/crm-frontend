d807be94552bb4a958b110dbe3380da4350ca6fb
Ahmad <primesurgetechnologies@gmail.com>
2023-11-27 17:08:15 +0500

    Projects converted to Orders

--- src/components/ListLayout.js
+++ src/components/ListLayout.js
@@ -54,7 +54,7 @@ export default function ListLayout() {
                     <List
                         subheader={
                             <ListSubheader component="div" id="nested-list-subheader">
-                                Client/Projects
+                                Client/Orders
                             </ListSubheader>
                         }
                     >                                  
