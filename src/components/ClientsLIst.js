79bc31eb510b3f37b7d88b0048fcef4a5722693c
Aneeq Tareen <aneeqtareen86@gmail.com>
2023-11-28 16:57:29 +0500

    ClientList change

--- src/components/ClientsLIst.js
+++ src/components/ClientsLIst.js
@@ -1,39 +1,59 @@
-import React from 'react'
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import React, { useState } from 'react';
 import { getDocs } from 'firebase/firestore';
-import { db, instancesRef, auth, } from '../config/firebase';
+import { db, instancesRef, auth } from '../config/firebase';
 import { collection } from 'firebase/firestore';
-import client from '../data/clients';
 import { useEffect } from 'react';
 import { setClients } from '../redux/reducers/clients';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
+import { styled } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
 
 const columns = [
-    { field: 'id', headerName: 'ID', flex: 1  },
-    { field: 'Name', headerName: ' Name', flex: 2, },
+    { field: 'id', headerName: 'ID', flex: 1 },
+    { field: 'Name', headerName: ' Name', flex: 2 },
     { field: 'email', headerName: 'Email', flex: 2 },
     {
         field: 'Contact',
         headerName: 'Contact',
-
-        flex: 2
-
+        flex: 2,
     },
-    
-
 ];
 
-
-
-export default function ClientsLIst() {
-
-    const clientsRef = collection(db, instancesRef + auth.currentUser.uid + "/client");
+export default function ClientsList() {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5); // Set your initial rows per page
+    const clientsRef = collection(db, instancesRef + auth.currentUser.uid + '/client');
     const clientList = useSelector((state) => state.clients.clients);
     const dispatch = useDispatch();
 
     const getClient = async () => {
-        //Read the Data
-        //Set the Movie List
         try {
             const data = await getDocs(clientsRef);
             const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
@@ -42,26 +62,58 @@ export default function ClientsLIst() {
             console.error(err);
         }
     };
+
     useEffect(() => {
         getClient();
     }, []);
 
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
     return (
-        <div style={{ height: 400, width: '100%', margin: "1%" }}>
-            {console.log("Data", clientList)}
-
-            <DataGrid
-                rows={clientList?.map((client) => ({ id: client.clientSid, email: client.email, Name: client.name, Contact: client.phone,}))}
-                columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
-                    },
-                }}
-                pageSizeOptions={[5, 10]}
-                checkboxSelection
-            />
-
+        <div style={{ height: 400, width: '100%', margin: '1%' }}>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>ID</StyledTableCell>
+                            <StyledTableCell align="left">Name</StyledTableCell>
+                            <StyledTableCell align="left">Email</StyledTableCell>
+                            <StyledTableCell align="left">Contact</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {(rowsPerPage > 0
+                            ? clientList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : clientList
+                        ).map((client) => (
+                            <StyledTableRow key={client.clientSid}>
+                                <TableCell component="th" scope="row">
+                                    {client.clientSid}
+                                </TableCell>
+                                <StyledTableCell align="left">{client.name}</StyledTableCell>
+                                <StyledTableCell align="left">{client.email}</StyledTableCell>
+                                <StyledTableCell align="left">{client.phone}</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={clientList.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </TableContainer>
         </div>
-    )
+    );
 }
--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -41,38 +41,8 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-        border: 0,
-    },
-}));
 
 
-const columns = [
-    { field: 'sid', headerName: 'ID', flex: 1, headerClassName: 'bold-header' },
-    {
-        field: 'companyName',
-        headerName: 'Company Name',
-
-        flex: 2
-
-    },
-    { field: 'ntn', headerName: ' NTN', flex: 2 },
-    { field: 'city', headerName: 'City', flex: 2 },
-    {
-        field: 'Contact',
-        headerName: 'Contact',
-
-        flex: 2
-
-    },
-
-
-];
 
 
 
